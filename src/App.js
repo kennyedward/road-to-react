e0365e804a98bbe1df83910522ec9d618f0a59d8
Kehinde Odewole <kennyedward99@gmail.com>
2021-03-04 09:05:34 +0100

    make changes to App & App.test files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+
+const useSemiPersistentState = () => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem("value") || ""
+  )
+  
+  React.useEffect(() => {
+    localStorage.setItem("value", value)
+  }, [setValue])
+
+  return [value, setValue]
+}
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
+      points: 4,
+      objectID: 0,
+  }, 
+  {
+      title: 'Redux',
+      url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+      points: 5,
+      objectID: 1,
+  }, 
+];
+
+const [searchTerm, setSearchTerm] = useSemiPersistentState()
+
+const handleSearch = event => {
+  const userSearch = event.target.value
+  setSearchTerm(userSearch)
+}
+
+const searchedStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title />
+      <Search onSearch={handleSearch} search={searchTerm} />
+      <hr />
+      <List list={searchedStories} />
     </div>
   );
 }
+
+const Title = () => {
+  return (
+    <h1>Hacker Stories</h1>
+  )
+}
+
+const List = ({ list }) => list.map(({ objectID, ...item }) => (
+    <Item key={objectID} {...item} />
+))
+
+const Item = ({ url, title, author, num_comments, points }) => (
+  <div>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </div>
+)
+
+const Search = ({onSearch, search}) => (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input 
+        type="text" 
+        id="search" 
+        onChange={onSearch} 
+        value={search} 
+      />
+    </div>
+  )
 
 export default App;
