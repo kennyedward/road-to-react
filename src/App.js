042e93a50ce8683aa0303cf975d3af202bd79e7f
Kehinde Odewole <kennyedward99@gmail.com>
2021-03-05 23:06:25 +0100

    add reducers

--- src/App.js
+++ src/App.js
@@ -33,9 +33,26 @@ const getAsyncStories = () => new Promise(resolve =>
     () => resolve({ data: { stories: initialStories }}), 2000)
   )
 
+const storiesReducer = (state, action) => {
+  // if (action.type === "SET_STORIES") {
+  //   return action.payload
+  // } else if (action.type === "REMOVE_STORY") {
+  //   return state.filter(story => action.payload.objectID !== story.objectID)
+  // } else throw new Error()
+  switch (action.type) {
+    case "SET_STORIES":
+      return action.payload
+    case "REMOVE_STORY":
+      return state.filter(story => action.payload.objectID !== story.objectID)
+    default:
+      throw new Error()
+  }
+}
+
 const App = () => {
 const [searchTerm, setSearchTerm] = useSemiPersistentState("Search", "React")
-const [stories, setStories] = React.useState([])
+// const [stories, setStories] = React.useState([])
+const [stories, dispatchStories] = React.useReducer(storiesReducer, [])
 const [isLoading, setIsLoading] = React.useState(false)
 const [isError, setIsError] = React.useState(false)
 
@@ -43,16 +60,24 @@ React.useEffect(() => {
   setIsLoading(true)
 
   getAsyncStories().then(result => {
-    setStories(result.data.stories)
+    // setStories(result.data.stories)
+    dispatchStories({
+      type: "SET_STORIES",
+      payload: result.data.stories
+    })
     setIsLoading(false)
   })
   .catch(() => setIsError(true))
 }, [])
 
 const handleRemoveStory = item => {
-  const newStories = stories.filter(story => item.objectID !== story.objectID)
+  // const newStories = stories.filter(story => item.objectID !== story.objectID)
 
-  setStories(newStories)
+  // setStories(newStories)
+  dispatchStories({
+    type: "REMOVE_STORY",
+    payload: item
+  })
 }
 
 const handleSearch = event => {
