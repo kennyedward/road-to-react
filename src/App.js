41f67b0f3251ec844f33a93b4e3642b8fa32fdbf
Kehinde Odewole <kennyedward99@gmail.com>
2021-03-06 22:00:08 +0100

    replace fetch with axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import './App.css';
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query="
@@ -47,22 +48,34 @@ const [isLoading, setIsLoading] = React.useState(false)
 const [isError, setIsError] = React.useState(false)
 const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`)
 
-const handleFetchStories = React.useCallback(() => {
+const handleFetchStories = React.useCallback(async () => {
   if (!searchTerm) return
   dispatchStories({ type: "STORIES_FETCH_INIT" })
 
-  fetch(url)
-    .then(response => response.json())
-    .then(result => {
-      dispatchStories({
-        type: "STORIES_FETCH_SUCCESS",
-        payload: result.hits
-      })
-    }, searchTerm)
-  .catch(() => {
+  try {
+    const result = await axios.get(url)
+
+    dispatchStories({
+      type: "STORIES_FETCH_SUCCESS",
+      payload: result.data.hits
+    })
+  } catch {
     dispatchStories({ type: "STORIES_FETCH_FAILURE" })
-  })
+  }
 }, [url])
+
+//   axios.get(url)
+//     .then(response => response.json())
+//     .then(result => {
+//       dispatchStories({
+//         type: "STORIES_FETCH_SUCCESS",
+//         payload: result.data.hits
+//       })
+//     }, searchTerm)
+//   .catch(() => {
+//     dispatchStories({ type: "STORIES_FETCH_FAILURE" })
+//   })
+// }, [url])
 
 React.useEffect(() => {
   handleFetchStories()
